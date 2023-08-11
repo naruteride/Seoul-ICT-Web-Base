d8a6ec35aaa4b224d83da321fd1b3fd95aea0a63
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-08-12 07:11:54 +0900

    Update timer.js

--- 타이머/timer.js
+++ 타이머/timer.js
@@ -11,3 +11,17 @@ let interval = setInterval(() => {
         clearInterval(interval);
     }
 }, 30000);
+
+// const today = new Date();
+// const yyyy = today.getFullYear();
+// const MM = (today.getMonth() + 1).toString().padStart(2, '0');
+// const dd = (today.getDate()).toString().padStart(2, '0');
+
+// const countDownDate = new Date(`${yyyy}-${MM}-${dd}T08:57:38`).getTime();
+
+// let interval = setInterval(() => {
+//     if (countDownDate - new Date().getTime() < 0) {
+//         document.querySelector(".Button_variant_primary__7ZJLT").click();
+//         clearInterval(interval);
+//     }
+// }, 30000);
