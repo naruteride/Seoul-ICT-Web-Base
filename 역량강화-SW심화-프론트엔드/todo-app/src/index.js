3352cfe86c55bfedd421a28feae7abc19b132718
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-08-11 18:56:05 +0900

    Organize folders

---
+++ 역량강화-SW심화-프론트엔드/todo-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Sample from './Sample';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Sample />
+);
+reportWebVitals();
--- 타이머/timer.js
+++ 타이머/timer.js
@@ -1,6 +1,13 @@
-let countDownDate = new Date("2023-08-01T12:45:42").getTime();
-setInterval(() => {
+const today = new Date();
+const yyyy = today.getFullYear();
+const MM = (today.getMonth() + 1).toString().padStart(2, '0');
+const dd = (today.getDate()).toString().padStart(2, '0');
+
+const countDownDate = new Date(`${yyyy}-${MM}-${dd}T12:45:42`).getTime();
+
+let interval = setInterval(() => {
     if (countDownDate - new Date().getTime() < 0) {
         document.querySelector(".preview-join-button").click();
+        clearInterval(interval);
     }
 }, 30000);
